2b51630674680ffc6490e7169cbf66a799d40408
AndrewH72 <andrewhsu72@gmail.com>
2026-03-26 23:27:40 -0700

    further processing of data, specifically the mouse data

---
+++ scripts/removeFiles.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+DIRECTORY="/Users/andrewhsu/Projects/McNair/data/gxdrnaseq"
+UNIQUE_TISSUES="/Users/andrewhsu/Projects/McNair/uniqueTissues.txt"
+HUMAN_TISSUES="/Users/andrewhsu/Projects/McNair/data/humanData.csv"
+
+> "$UNIQUE_TISSUES"
+echo "Starting file processing in $DIRECTORY."
+echo "Reading human tissues."
+
+awk -F',' 'NR > 1 && !seen[$NF]++ {print $NF}' "$HUMAN_TISSUES" >> "$UNIQUE_TISSUES"
+echo "Processing gxdrnaseq files."
+find "$DIRECTORY" -type f -print0 | while IFS= read -r -d '' file; do
+    value=$(awk -F'|' 'NR > 1 {print $6; exit}' "$file")
+    if grep -F -x -q "$value" "$UNIQUE_TISSUES"; then
+        echo "Deleting $file"
+        rm "$file"
+    fi
+done
+rm "$UNIQUE_TISSUES"
+echo "Done."
